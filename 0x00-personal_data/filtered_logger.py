61128282df64ef8cc85d95e88f620a7b6d47f123
merytpeters <merytpeters@gmail.com>
2024-11-07 13:39:50 +0100

    Regexing, trying to meet requirements

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,12 +4,11 @@ import re
 
 
 def filter_datum(
-    fields: list[str], redaction: str, message: str, separator: str
-) -> str:
+        fields: list[str], redaction: str, message: str, separator: str
+        ) -> str:
     """Returns the log message obsfucated"""
-    pattern = f"({
-        '|'.join(re.escape(field) + r'=[^' + re.escape(separator) + r']*'
-                 for field in fields)})"
     return re.sub(
-        pattern, lambda m: f"{m.group(1).split('=')[0]}={redaction}", message
-    )
+        f"({'|'.join(re.escape(field) +
+                     r'=[^' + re.escape(separator) + r']*'
+                     for field in fields)})",
+        lambda m: f"{m.group(1).split('=')[0]}={redaction}", message)
