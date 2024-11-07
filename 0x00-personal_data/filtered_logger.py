8a1d3c4237f6045d5f8d46275b2d8c04f6161733
merytpeters <merytpeters@gmail.com>
2024-11-07 13:16:33 +0000

    Log Formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Regex-ing"""
+"""Log Formatter"""
+import logging
 import re
 from typing import List
 
@@ -11,3 +12,20 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                         (separator) + r']*' for field in fields])
     return re.sub(r'(' + pattern + r')', lambda m:
                   f"{m.group(0).split('=')[0]}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message, ";")
