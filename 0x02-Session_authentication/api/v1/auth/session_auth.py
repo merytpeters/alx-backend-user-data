0f69097fca3a35527cfbc1c43aeda03bb3c52dcc
merytpeters <merytpeters@gmail.com>
2024-11-14 22:53:07 +0100

    trying to meet requirements

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Session Authentication"""
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
     """Session Auth Class"""
-    
