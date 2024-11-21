d1420d0fd2813cac7f9f5e7b8433610d6254a252
merytpeters <merytpeters@gmail.com>
2024-11-22 00:20:45 +0100

    Delete session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,3 +72,7 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy Session"""
+        self._db.update_user(user_id, session_id=None)
