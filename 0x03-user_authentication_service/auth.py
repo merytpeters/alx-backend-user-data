d9579278a758e5dcc866ffcd2db642007a8bd569
merytpeters <merytpeters@gmail.com>
2024-11-21 23:17:08 +0100

    Credentials Validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -36,3 +36,12 @@ class Auth:
             new_user = self._db.add_user(email, hashed_password)
 
         return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation"""
+        try:
+            user = self._db.find_user_by(email=email)
+            stored_password = user.hashed_password
+            return bcrypt.checkpw(password.encode(), stored_password)
+        except NoResultFound:
+            return False
