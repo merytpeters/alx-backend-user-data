67f6c6759d63565c9506042b23fdaf087a2bc7ab
merytpeters <merytpeters@gmail.com>
2024-11-21 12:23:53 +0100

    Nullable strings updated

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,5 +13,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(250), unique=True, nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), default=None)
-    reset_token = Column(String(250), default=None)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
