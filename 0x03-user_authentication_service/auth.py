63df5b4f892c00e9b142e4e994ae2eceb470b219
merytpeters <merytpeters@gmail.com>
2024-11-21 23:42:31 +0100

    Moved _generate_uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,6 +19,11 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+def _generate_uuid() -> str:
+    """Returns a string representation of a new UUID"""
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -47,15 +52,11 @@ class Auth:
         except NoResultFound:
             return False
 
-    def _generate_uuid(self) -> str:
-        """Returns a string representation of a new UUID"""
-        return str(uuid.uuid4())
-
     def create_session(self, email: str) -> str:
         """Get Session ID"""
         try:
             user = self._db.find_user_by(email=email)
-            session_id = self._generate_uuid()
+            session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
 
             return session_id
