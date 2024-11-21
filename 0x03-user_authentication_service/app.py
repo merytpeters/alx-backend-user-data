295f91fb4f4af357a17f2842dd0586dc8f1298b9
merytpeters <merytpeters@gmail.com>
2024-11-22 00:55:15 +0100

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -91,7 +91,7 @@ def get_reset_password_token():
 
     try:
         reset_token = AUTH.get_reset_password_token(email)
-        return jsonify(email=email, reset_token="reset_token"), 200
+        return jsonify(email=email, reset_token=reset_token), 200
     except ValueError:
         abort(403)
 
