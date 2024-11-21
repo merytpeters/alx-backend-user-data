408c76fa64c664d95ccba28912deae9568a1c037
merytpeters <merytpeters@gmail.com>
2024-11-21 12:59:31 +0100

    Create User, pycodestyle error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email:str, hashed_password:str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add User"""
         if email and hashed_password:
             new_user = User(email=email, hashed_password=hashed_password)
