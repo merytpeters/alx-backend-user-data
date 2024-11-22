88df8dd9db915182f61d8c339933b8cd22f7144a
merytpeters <merytpeters@gmail.com>
2024-11-22 01:11:26 +0100

    Update password, error fixed

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -89,7 +89,7 @@ class Auth:
             raise ValueError(
                 "User not found or unable to generate reset password token")
 
-    def update_passsword(self, reset_token: str, password: str) -> None:
+    def update_password(self, reset_token: str, password: str) -> None:
         """Update Password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
