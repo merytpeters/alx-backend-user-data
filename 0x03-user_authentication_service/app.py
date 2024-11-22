c5f4715b909bea139f48b499b97ee3b8bd05cd7f
merytpeters <merytpeters@gmail.com>
2024-11-22 01:22:32 +0100

    Update password endpoint, trying to meet requirement

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -104,9 +104,6 @@ def update_password():
         reset_token = request.form.get("reset_token")
         new_password = request.form.get("new_password")
 
-        if not email or not reset_token or not new_password:
-            abort(403)
-
         AUTH.update_password(reset_token, new_password)
 
         return jsonify(email=email, message="Password updated"), 200
